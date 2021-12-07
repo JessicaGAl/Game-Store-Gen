ca17ee37b264b26220545006d1d671506e1f5f47
Matheus Gomes <mgrspeed@gmail.com>
2021-12-07 14:11:34 -0300

    @Rorschach616 [Task: Add ProdutoModel]

--- src/main/java/com/GameStoreGen/GameStoreGen/models/ProdutoModel.java
+++ src/main/java/com/GameStoreGen/GameStoreGen/models/ProdutoModel.java
@@ -1,5 +1,105 @@
 package com.GameStoreGen.GameStoreGen.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tbl_produto")
 public class ProdutoModel {
+
+	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idProduto;
+	private @NotBlank String titulo;
+	private @NotBlank String sinopse;
+	private @NotBlank Double preco;
+	private @NotBlank boolean estoque;
+	private @NotBlank int idade;
+	private @NotBlank String plataforma;
+	private @NotBlank boolean popular;
+	private @NotBlank boolean online;
+	private @NotBlank Long fk_categoria;
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getSinopse() {
+		return sinopse;
+	}
+
+	public void setSinopse(String sinopse) {
+		this.sinopse = sinopse;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public boolean isEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(boolean estoque) {
+		this.estoque = estoque;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public boolean isPopular() {
+		return popular;
+	}
+
+	public void setPopular(boolean popular) {
+		this.popular = popular;
+	}
+
+	public boolean isOnline() {
+		return online;
+	}
+
+	public void setOnline(boolean online) {
+		this.online = online;
+	}
+
+	public Long getFk_categoria() {
+		return fk_categoria;
+	}
+
+	public void setFk_categoria(Long fk_categoria) {
+		this.fk_categoria = fk_categoria;
+	}
 
 }

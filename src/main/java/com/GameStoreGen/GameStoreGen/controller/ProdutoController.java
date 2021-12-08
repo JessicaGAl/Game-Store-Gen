a0fa4f9b93995f1ad74bc936ef76e03a80259692
VCatucci <93915673+VCatucci@users.noreply.github.com>
2021-12-08 11:11:40 -0300

    @VCatucci[Task: Add Controller]

--- src/main/java/com/GameStoreGen/GameStoreGen/controller/ProdutoController.java
+++ src/main/java/com/GameStoreGen/GameStoreGen/controller/ProdutoController.java
@@ -5,11 +5,18 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.GameStoreGen.GameStoreGen.models.CategoriaModel;
 import com.GameStoreGen.GameStoreGen.models.ProdutoModel;
 import com.GameStoreGen.GameStoreGen.repository.ProdutoRepository;
 
@@ -17,16 +24,26 @@ import com.GameStoreGen.GameStoreGen.repository.ProdutoRepository;
 @RequestMapping("/api/v1/produto")
 public class ProdutoController {
 
-	public @Autowired ProdutoRepository produto;
+	public @Autowired ProdutoRepository repository;
 
 	public @GetMapping("/all") List<ProdutoModel> findAll() {
 
-		return produto.findAll();
+		return repository.findAll();
 	}
 
-	public @PostMapping("/save") ProdutoModel save(@Valid @RequestBody ProdutoModel newProd) {
-
-		return produto.save(newProd);
-
+	@PostMapping("/save")
+	public ResponseEntity<ProdutoModel> save(@Valid @RequestBody ProdutoModel newProd) {
+		return ResponseEntity.status(201).body(repository.save(newProd));
 	}
+
+	@PutMapping("/update")
+	public ResponseEntity<ProdutoModel> put(@Valid @RequestBody ProdutoModel produto) {
+		return ResponseEntity.status(200).body(repository.save(produto));
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+
 }
